8f9fc7e939a9fa9a31f002ec3daf291088de1996
LunarHackett003 <61294652+LunarHackett003@users.noreply.github.com>
2024-10-23 14:39:55 +0100

    Clean Slate

--- Assets/Scripts/Utility/INetworkPrefabInstanceHandler.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Netcode;
-using UnityEngine;
-public interface INetworkPrefabInstanceHandler
-{
-    NetworkObject Instantiate(ulong ownerClientId, Vector3 position, Quaternion rotation);
-    void Destroy(NetworkObject networkObject);
-}
-
